3161b3c08151a11915f9c240ca895bf9d1e7c93b
kumail <alikumail1100@gmail.com>
2022-11-03 19:42:58 +0500

    tenth

--- src/components/Shares.js
+++ src/components/Shares.js
@@ -7,23 +7,23 @@ const Shares = () => {
             <div className="container ">
                 <div className="share share1">
                     <h3>Team</h3>
-                    <h2>1%</h2>
+                    <h2>10%</h2>
 
                     <div className="area"></div>
                 </div>
                 <div className="share share2">
                     <h3>Marketing</h3>
-                    <h2>1%</h2>
+                    <h2>10%</h2>
                     <div className="area"></div>
                 </div>
                 <div className="share share3">
                     <h3>Advisors(for DAO)</h3>
-                    <h2>1%</h2>
+                    <h2>10%</h2>
                     <div className="area"></div>
                 </div>
                 <div className="share share4">
                     <h3>ICO(DAO FUNDS)</h3>
-                    <h2>97%</h2>
+                    <h2>70%</h2>
                     <div className="area"></div>
                 </div>
             </div>
